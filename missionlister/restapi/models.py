513f34d51888b3a1e4aa05a8ca7dd09ba80b1225
Felix Mycka <felix04m@gmail.com>
2024-10-29 15:14:43 +0100

    added the first addpoint and added urls

--- missionlister/restapi/models.py
+++ missionlister/restapi/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Mission(models.Model): 
+    # our datastructure is defined here
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=1000) # TODO: what should max_length be?
+    date = models.DateTimeField()
+    location = models.CharField(max_length=1000)
+    other = models.CharField(max_length=1000)
+
+    # this function defines, what the value of print(mission) would be
+    def __str__(self):
+        return self.id # TODO: id or name?
