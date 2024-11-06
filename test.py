9f4a638f5a3e4ec99599b8a1fb4d435ac3fd224b
Felix Mycka <felix04m@gmail.com>
2024-11-06 17:17:48 +0100

    fixed formatting, should get accepted

--- test.py
+++ test.py
@@ -1,12 +1,27 @@
 import random
 
-CHARACTERS = ("Frodo", "Sam", "Merry", "Pippin", "Aragorn", "Legolas", "Gimli", "Boromir", "Gandalf", "Saruman", "Sauron")
+CHARACTERS = (
+    "Frodo",
+    "Sam",
+    "Merry",
+    "Pippin",
+    "Aragorn",
+    "Legolas",
+    "Gimli",
+    "Boromir",
+    "Gandalf",
+    "Saruman",
+    "Sauron",
+)
+
 
 def random_character():
     return random.choice(CHARACTERS)
 
+
 def ring_bearer(name):
     return name in ("Frodo", "Sam")
+
 
 if __name__ == "__main__":
     character = random_character()
