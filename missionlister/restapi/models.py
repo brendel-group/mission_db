2beb88de2e499f50dfc0c0e66b2bd94f32c17ed7
Pascal Kuppler <pkuppler26@gmail.com>
2024-11-06 18:39:36 +0100

    Edit models.py to include tags and mission_tags tables

--- missionlister/restapi/models.py
+++ missionlister/restapi/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Mission(models.Model):
     # our datastructure is defined here
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=65536)
     date = models.DateField()
     location = models.CharField(max_length=65536)
@@ -13,3 +13,14 @@ class Mission(models.Model):
     # this function defines, what the value of print(mission) would be
     def __str__(self):
         return self.id
+
+
+class Tag(models.Model):
+    '''The tags table'''
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=65536)
+
+class Mission_tags(models.Model):
+    '''The relationship table to connect Misson and Tag'''
+    mission_id = models.ForeignKey(Mission, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
