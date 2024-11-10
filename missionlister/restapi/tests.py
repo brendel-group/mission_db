fe6fa16fb2a7fb0f80dfb9c69a51c958ba8743c1
Pascal Kuppler <pkuppler26@gmail.com>
2024-11-10 19:11:50 +0100

    Add Tests for Tags and Mission_tags

--- missionlister/restapi/tests.py
+++ missionlister/restapi/tests.py
@@ -1,3 +1,171 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from django.db.utils import IntegrityError
+from django.utils import timezone
+from .models import *
+from .views import *
+from .serializer import *
+from datetime import datetime
 
 # Create your tests here.
+
+class TagTestCase(TestCase):
+    test_names = list()
+    def setUp(self):
+        test_number = list(range(0,5))
+        for n in test_number:
+            while Tag.objects.filter(name="test"+str(n)).exists():
+                n += 1
+            Tag.objects.create(name="test"+str(n))
+            self.test_names.append("test"+str(n))
+    
+    def test_structure(self):
+        self.assertEqual(
+            len(Tag.objects.filter(name__in=self.test_names)),
+            5, 
+            "Test if 5 tags where created")
+        
+    def test_unique_violation(self):
+        # use an existing name
+        name = self.test_names[0]
+        
+        # and a new id
+        id = Tag.objects.get(name=name).id+1
+        while Tag.objects.filter(id=id).exists():
+            id += 1
+
+        # should raise an exception because name is unique
+        self.assertRaises(
+            IntegrityError,
+            Tag.objects.create, id=id, name=name)
+        
+class RestAPITagTestCase(APITestCase):
+
+    test_names = list()
+    def setUp(self):
+        test_number = list(range(0,5))
+        for n in test_number:
+            while Tag.objects.filter(name="test"+str(n)).exists():
+                n += 1
+            Tag.objects.create(name="test"+str(n))
+            self.test_names.append("test"+str(n))
+
+    def test_get_tags(self):
+        response = self.client.get(reverse('get_tags'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data),5)
+        self.assertEqual(
+            response.data,
+            [
+                {'id': Tag.objects.get(name=self.test_names[0]).id, 'name': self.test_names[0]},
+                {'id': Tag.objects.get(name=self.test_names[1]).id, 'name': self.test_names[1]},
+                {'id': Tag.objects.get(name=self.test_names[2]).id, 'name': self.test_names[2]},
+                {'id': Tag.objects.get(name=self.test_names[3]).id, 'name': self.test_names[3]},
+                {'id': Tag.objects.get(name=self.test_names[4]).id, 'name': self.test_names[4]}            
+            ]
+        )
+
+    def test_create_tag(self):
+        response = self.client.post(
+            reverse('create_tag'),
+            { 'name': "test_create_tag"},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data),2)
+        self.assertEqual(response.data,
+                         { 'id': Tag.objects.get(name="test_create_tag").id, 'name': "test_create_tag"})
+
+    def test_create_tag_with_existing_name(self):
+        existing_name = self.test_names[0]
+        response = self.client.post(
+            reverse('create_tag'),
+            {'name': existing_name},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data,
+            {"name": ["tag with this name already exists."]}
+        )
+
+    def test_tag_detail_get(self):
+        response = self.client.get(
+            reverse(tag_detail,
+                    kwargs={'name': self.test_names[0]}),
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,
+                         { 'id': Tag.objects.get(name=self.test_names[0]).id,
+                          'name': self.test_names[0]})
+
+    def test_tag_detail_put(self):
+        response = self.client.put(
+            reverse(tag_detail,
+                    kwargs={'name': self.test_names[0]}),
+            { 'name': 'test_detail_put'},
+            format='json')
+        self.test_names[0] = "test_detail_put"
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,
+                         { 'id': Tag.objects.get(name="test_detail_put").id,
+                          'name': "test_detail_put"})
+        
+    def test_tag_detail_delete(self):
+        response = self.client.delete(
+            reverse(tag_detail,
+                    kwargs={'name': self.test_names[0]}))
+        del self.test_names[0]
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class RestAPIMissionTagsTestCase(APITestCase):
+    def setUp(self):
+        # Create test data
+        self.mission = Mission.objects.create(
+            name="TestMission",
+            date=timezone.now(),
+            location="TestLocation",
+            other="TestOther"
+        )
+        self.tags = []
+        for i in range(3):
+            tag = Tag.objects.create(name=f"TestTag{i}")
+            self.tags.append(tag)
+        
+        # Create initial tag associations
+        self.mission_tag = Mission_tags.objects.create(mission=self.mission, tag=self.tags[0])
+
+    def test_add_tag_to_mission(self):
+        # Add a new tag to a mission using the API
+        data = {'mission_id': self.mission.id, 'tag_name': "TestTag1"}
+        response = self.client.post(reverse('add_tag_to_mission'), data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Check if the mission is now associated with the new tag
+        self.assertTrue(Mission_tags.objects.filter(mission=self.mission, tag=self.tags[1]).exists())
+        
+    def test_delete_mission_tag(self):
+        # Remove tag association using the API
+        response = self.client.delete(reverse('delete_mission_tag', kwargs={'mission_id': self.mission.id, 'tag_name': self.tags[0].name}))
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        # Check if the mission-tag association is removed
+        self.assertFalse(Mission_tags.objects.filter(mission=self.mission, tag=self.tags[0]).exists())
+
+    def test_get_missions_by_tag(self):
+        # Check missions related to a specific tag
+        response = self.client.get(reverse('get_missions_by_tag', kwargs={'name': 'TestTag0'}))
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['id'], self.mission.id)
+
+    def test_get_tags_by_mission_id(self):
+        # Check tags associated with a mission
+        response = self.client.get(reverse('get_tags_by_mission_id', kwargs={'id': self.mission.id}))
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], self.tags[0].name)
