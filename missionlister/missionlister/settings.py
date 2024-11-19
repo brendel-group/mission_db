736f76a06faa94715faccd6c86c32ba15c736635
KageKrito <148358692+KageKrito@users.noreply.github.com>
2024-11-19 13:52:38 +0100

    FetchAPI to fetch data from backend (#33)
    This PR contains the implementation of the fetch API
    Resolves #7
    #
    To use random_table_data from RandomData instead of fetching data from
    the backend, just navigate to frontend/app/utilities/fetchapi.ts and set
    useRandomData to true
    #
    #### New files:
    fetchapi.ts:
    - Added utils/fetchapi.ts to frontend/app for interaction with the
    endpoints of the REST API (GET, GET by ID, POST, PUT, DELETE)
    - Added option useRandomData
    
    #### Changed files:
    Overview.tsx:
    - Added useEffect to Overview.tsx in frontend/app/pages/missions to
    fetch the data from the backend
    - Also added a loading indicator and an error viewer as HTML paragraphs
    to Overview.tsx
    #
    data.tsx
    - Added Interface BackendMissionData to data.tsx for the fetched data to
    handle the difference to the fields in the backend
    #
    fetchapi.ts:
    - Added credentials to each method in fetchapi.ts
    - Added method fetchAndTransformMissions to fetchapi.ts to transform
    data from the BackendMissionData interface to the MissionData interface
    #
    settings.py:
    - Added Django CORS headers to missionlister/missionlister/settings.py
    #
    requirements.txt:
    - Added django-cors-headers to requirements.txt

--- missionlister/missionlister/settings.py
+++ missionlister/missionlister/settings.py
@@ -45,17 +45,26 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "restapi",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "missionlister.urls"
 
