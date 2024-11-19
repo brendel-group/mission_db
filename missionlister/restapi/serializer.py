d7aab60811ff70a11d96cbd0f556462ecf273fa4
KageKrito <oliver.heckel2002@gmail.com>
2024-11-19 16:44:11 +0100

    Add serializers for mission files

--- missionlister/restapi/serializer.py
+++ missionlister/restapi/serializer.py
@@ -18,18 +18,13 @@ class FileSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class MissionFileSerializer(serializers.ModelSerializer):
-    file = FileSerializer(read_only=True)
+    mission_id = serializers.IntegerField(source="mission.id", initial=None)
+    file_id = serializers.IntegerField(source="file.id", initial=None)
+    type = serializers.IntegerField(source="file.type", initial=None)
 
     class Meta:
         model = Mission_files
-        fields = "__all__"
-
-class MissionDetailSerializer(serializers.ModelSerializer):
-    files = serializers.SerializerMethodField()
-    
-    class Meta:
-        model = Mission
-        fields = ['id', 'name', 'date', 'location', 'other', 'files']
+        fields = ["mission_id", "file_id", "type"]
 
 
 class TagSerializer(serializers.ModelSerializer):
