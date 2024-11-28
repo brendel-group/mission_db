657d054d45a553cb12475d782682b8dd629a3d97
Pascal Kuppler <pkuppler26@gmail.com>
2024-11-28 21:53:22 +0100

    ruff format

--- missionlister/cli.py
+++ missionlister/cli.py
@@ -13,7 +13,7 @@ django.setup()
 
 # Importing Models, adjust as needed
 from restapi.models import Mission  # noqa
-from restapi.serializer import MissionSerializer # noqa
+from restapi.serializer import MissionSerializer  # noqa
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -45,10 +45,11 @@ def validate_date(date_str):
         logging.error("Date and time format should be YYYY-MM-DD.")
         return None
 
+
 def print_table(list_of_dict):
     if not list_of_dict:
         return
-    
+
     keys = list(list_of_dict[0])
 
     # get widths of columns
@@ -56,7 +57,7 @@ def print_table(list_of_dict):
     widths = {}
 
     for key in keys:
-        list_of_widths = list(map(lambda d: len(str(d[key])),list_of_dict))
+        list_of_widths = list(map(lambda d: len(str(d[key])), list_of_dict))
         list_of_widths.append(len(key))
         widths[key] = max(list_of_widths)
 
@@ -68,7 +69,7 @@ def print_table(list_of_dict):
 
     header = header[:-3]
     header += "\n"
-    line = "-"*len(header)
+    line = "-" * len(header)
     header += line
     print(header)
 
@@ -80,6 +81,7 @@ def print_table(list_of_dict):
             line += f"{str(entry[key]):<{widths[key]}} | "
         line = line[:-3]
         print(line)
+
 
 # Add mission to DB
 def add_mission_from_folder(folder_path, location, other):
@@ -100,7 +102,9 @@ def add_mission_from_folder(folder_path, location, other):
 # Add mission to DB
 def add_mission(name, date, location, other):
     try:
-        mission = Mission.objects.create(name=name, date=date, location=location, other=other)
+        mission = Mission.objects.create(
+            name=name, date=date, location=location, other=other
+        )
         mission.save()
         logging.info(f"'{name}' added.")
     except Exception as e:
@@ -116,6 +120,7 @@ def remove_mission(mission_id):
     except Mission.DoesNotExist:
         print(f"No mission found with ID {mission_id}.")
 
+
 def mission_arg_parser(subparser):
     mission_parser = subparser.add_parser("mission", help="Modify Missions")
     mission_subparser = mission_parser.add_subparsers(dest="command")
@@ -124,12 +129,8 @@ def mission_arg_parser(subparser):
     add_parser = mission_subparser.add_parser("add", help="Add mission")
     add_parser.add_argument("--name", required=True, help="Mission name")
     add_parser.add_argument("--date", required=True, help="Mission date (YYYY-MM-DD)")
-    add_parser.add_argument(
-        "--location", required=False, help="Mission location"
-    )
-    add_parser.add_argument(
-        "--other", required=False, help="Other mission details"
-    )
+    add_parser.add_argument("--location", required=False, help="Mission location")
+    add_parser.add_argument("--other", required=False, help="Other mission details")
 
     # Remove command
     remove_parser = mission_subparser.add_parser("remove", help="Remove mission")
@@ -140,15 +141,12 @@ def mission_arg_parser(subparser):
 
     return mission_parser
 
+
 def folder_arg_parser(subparser):
     folder_parser = subparser.add_parser("addfolder", help="adds details from folder")
     folder_parser.add_argument("--path", required=True, help="Filepath")
-    folder_parser.add_argument(
-        "--location", required=False, help="location"
-    )
-    folder_parser.add_argument(
-        "--other", required=False, help="other mission details"
-    )
+    folder_parser.add_argument("--location", required=False, help="location")
+    folder_parser.add_argument("--other", required=False, help="other mission details")
 
 
 def main():
@@ -164,7 +162,7 @@ def main():
 
     # Execute command
     match args.type:
-        case "mission": 
+        case "mission":
             match args.command:
                 case "add":
                     # Validate date
