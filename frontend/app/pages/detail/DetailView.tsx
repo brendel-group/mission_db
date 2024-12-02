243a017a3ba3dbc73cbb7958b0e3a3962bf19095
Simon Rappenecker <simi2657@gmail.com>
2024-12-02 13:48:31 +0100

    Rebased to main

--- frontend/app/pages/detail/DetailView.tsx
+++ frontend/app/pages/detail/DetailView.tsx
@@ -1,4 +1,4 @@
-import { Modal, Container, Grid, Text, rem } from "@mantine/core";
+import { Container, Grid, Text, rem } from "@mantine/core";
 import React from "react";
 import { RenderTagsDetailView } from "../../utilities/TagList";
 import { ShowDatasets } from "./DatasetTable";
@@ -6,63 +6,58 @@ import { ShowStatsView } from "./StatsView";
 import { MissionData } from "~/data";
 import { detail_view_data } from "~/RandomData";
 
-interface DetailViewProps {
-  modalOpened: boolean;
+interface DetailsViewProps {
   missionData: MissionData | null;
-  onClose: () => void;
 }
 
-const RenderView: React.FC<DetailViewProps> = ({
-  modalOpened,
-  missionData,
-  onClose,
-}) => {
-  const detailViewData = missionData
-    ? detail_view_data[missionData.missionId]
+const DetailView: React.FC<DetailsViewProps> = ({ missionData: selectedRow }) => {
+  const detailViewData = selectedRow
+    ? detail_view_data[selectedRow.missionId]
     : null;
 
   return (
-    <Modal
-      opened={modalOpened}
-      onClose={onClose}
-      title={
-        missionData
-          ? `${missionData.name}, ${missionData.location} with ${missionData.robot}`
-          : "Mission Details"
-      }
-      fullScreen
-      styles={{
-        title: {
-          fontSize: "30px",
-        },
+    <Container
+      my="md"
+      style={{
+        maxWidth: "100%", // Aligns it to the left
+        padding: "0 1rem", // Adds padding to the container
       }}
     >
-      <Container my="md" mx={0} px={0}>
-        <Grid gutter="md">
-          {/* Left column occupying 80% */}
-          <Grid.Col span={9}>
-            <Grid>
-              {/* Tags */}
-              <Grid.Col span={12}>
-                {missionData && (
-                  <RenderTagsDetailView tags={missionData.tags} />
-                )}
-              </Grid.Col>
-              {/* Table */}
-              <Grid.Col span={12}>
-                {detailViewData && <ShowDatasets data={detailViewData} />}
-              </Grid.Col>
-            </Grid>
-          </Grid.Col>
+      {/* Title */}
+      <Text
+        size={rem(30)}
+        style={{ marginBottom: "1rem" }}
+      >
+        {selectedRow
+          ? `${selectedRow.name}, ${selectedRow.location} with ${selectedRow.robot}`
+          : "Mission Details"}
+      </Text>
 
-          {/* Stats */}
-          <Grid.Col span={3}>
-            <ShowStatsView selectedRow={missionData} />
-          </Grid.Col>
-        </Grid>
-      </Container>
-    </Modal>
+      {/* Main content */}
+      <Grid gutter="md">
+        {/* Left column occupying 80% */}
+        <Grid.Col span={9}>
+          <Grid gutter="md">
+            {/* Tags */}
+            <Grid.Col span={12}>
+              {selectedRow && (
+                <RenderTagsDetailView tags={selectedRow.tags} />
+              )}
+            </Grid.Col>
+            {/* Table */}
+            <Grid.Col span={12}>
+              {detailViewData && <ShowDatasets data={detailViewData} />}
+            </Grid.Col>
+          </Grid>
+        </Grid.Col>
+
+        {/* Stats */}
+        <Grid.Col span={3}>
+          <ShowStatsView missionData={selectedRow} />
+        </Grid.Col>
+      </Grid>
+    </Container>
   );
 };
 
-export default RenderView;
+export default DetailView;
