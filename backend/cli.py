a1103cfa52a8ed64ad61a917ed2d8c49b9f8c041
pascal260303 <75665115+pascal260303@users.noreply.github.com>
2024-12-05 19:18:11 +0100

    CLI add basic functionality for Tags (#95)
    I addded the commands `tag add`, `tag remove`, `tag list` and `tag
    change` to the CLI to provide basic functionality for Tags.\
    It is at the moment not possible to add a Tag to a Mission, list Tags by
    Mission or list Missions by Tag. That will be added in the next PR.
    
    resolves Task 1 of #20 
    
    ## Changes:
    - in `backend/cli.py`: add functions to add, remove, list and change
    Tags and adjusted the main function and the Argument parser accordingly.
    - in `backend/tests_cli.py`: add tests for added functionality
    - in `backend/restapi/models.py`: add string representation for Tag
    - in `docs/cli/README.md`: add documentation for new commands. Can be
    found
    [here](https://github.com/brendel-group/polybot_mission_db/blob/feature/cli-tags-basic/docs/cli/README.md)
    
    ## How to test:
    Execute all django tests with:
    ```bash
    ./manage.py test
    ```
    or only the CLI tests with:
    ```bash
    ./manage.py test tests_cli
    ```
    Read the
    [documentation](https://github.com/brendel-group/polybot_mission_db/blob/feature/cli-tags-basic/docs/cli/README.md)
    and try to use the CLI

--- backend/cli.py
+++ backend/cli.py
@@ -11,8 +11,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")  # Adjust as
 django.setup()
 
 # Importing Models, adjust as needed
-from restapi.models import Mission  # noqa
-from restapi.serializer import MissionSerializer  # noqa
+from restapi.models import Mission, Tag  # noqa
+from restapi.serializer import MissionSerializer, TagSerializer  # noqa
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -155,9 +155,8 @@ def add_mission(name, mission_date, location=None, other=None):
     other: optional string containing other extra information 
     """
     try:
-        mission = Mission.objects.create(
-            name=name, date=mission_date, location=location, other=other
-        )
+        mission = Mission(name=name, date=mission_date, location=location, other=other)
+        mission.full_clean()  # validation
         mission.save()
         logging.info(f"'{name}' added.")
     except Exception as e:
@@ -177,6 +176,111 @@ def remove_mission(mission_id):
         print(f"Mission with ID {mission_id} has been removed.")
     except Mission.DoesNotExist:
         print(f"No mission found with ID {mission_id}.")
+
+
+def add_tag(name, color=None):
+    """Add a tag to the database
+
+    Args:
+        name: Tag name
+        color (optional): Tag color
+    """
+    if color:
+        tag = Tag(name=name, color=color)
+    else:
+        tag = Tag(name=name)
+    try:
+        tag.full_clean()  # validation
+        tag.save()
+        logging.info(f"'{name}' Tag added.")
+    except Exception as e:
+        logging.error(f"Error adding Tag: {e}")
+
+
+def remove_tag(id=None, name=None):
+    """Remove a tag either by id or name. If both are given the id will be used and the name is ignored
+    logs an error if neither are given
+
+    Args:
+        id (optional): id of tag to remove
+        name (optional): name of tag to remove
+    """
+    if not (id or name):
+        logging.error("No id or name given")
+        return
+
+    try:
+        if id:
+            tag = Tag.objects.get(id=id)
+        else:
+            tag = Tag.objects.get(name=name)
+        tag.delete()
+        logging.info(f"Tag '{tag.name}' has been removed.")
+    except Tag.DoesNotExist:
+        if id:
+            logging.error(f"No Tag found with id '{id}'.")
+        else:
+            logging.error(f"No Tag found with name '{name}'.")
+
+
+def list_tags():
+    """
+    list all Tags in a table
+    """
+    tags = Tag.objects.all()
+    serializer = TagSerializer(tags, many=True)
+    print_table(serializer.data)
+
+
+def change_tag(id=None, name=None, color=None):
+    """
+    Change a Tag either by id or name. \\
+    If the id is given then always the Tag with that id will be changed.\\
+    If only the name is given the Tag will be identified by the name.
+
+    Args:
+        id (optional): the id of the Tag
+        name (optional): the name or the new name of the Tag
+        color (optional): the new color of the Tag
+    """
+    if not (id or name):
+        logging.error("At least one of --id or --name must be provided")
+        return
+    if not (name or color):
+        logging.info("Nothing to change")
+        return
+    try:
+        if id:
+            tag = Tag.objects.get(id=id)
+        elif name:
+            tag = Tag.objects.get(name=name)
+        else:
+            logging.error("At least one of --id or --name must be provided")
+            return
+
+        if name:
+            tag.name = name
+        if color:
+            tag.color = color
+        tag.full_clean()
+        tag.save()
+        logging.info(f"Tag changed to '{tag}'")
+    except Exception as e:
+        logging.error(f"Error changing Tag: {e}")
+
+
+def tag_command(tag_parser, args):
+    match args.command:
+        case "add":
+            add_tag(args.name, args.color)
+        case "remove":
+            remove_tag(args.id, args.name)
+        case "list":
+            list_tags()
+        case "change":
+            change_tag(args.id, args.name, args.color)
+        case _:
+            tag_parser.print_help()
 
 
 def mission_arg_parser(subparser):
@@ -219,6 +323,34 @@ def folder_arg_parser(subparser):
     folder_parser.add_argument("--other", required=False, help="other mission details")
 
 
+def tag_arg_parser(subparser):
+    tag_parser = subparser.add_parser("tag", help="Modify Tags")
+    tag_subparser = tag_parser.add_subparsers(dest="command")
+
+    # Add command
+    add_parser = tag_subparser.add_parser("add", help="Add mission")
+    add_parser.add_argument("--name", required=True, help="Tag name")
+    add_parser.add_argument("--color", required=False, help="Tag color")
+
+    # Remove command
+    remove_parser = tag_subparser.add_parser("remove", help="Remove Tag")
+    remove_parser.add_argument("--id", required=False, help="id of Tag to remove")
+    remove_parser.add_argument("--name", required=False, help="name of Tag to remove")
+
+    # remove_parser.add_argument("--id", required=True, help="ID")
+
+    # List command
+    _ = tag_subparser.add_parser("list", help="List all Tags")
+
+    # change command
+    change_parser = tag_subparser.add_parser("change", help="Change Tag by name or id")
+    change_parser.add_argument("--id", required=False, help="Tag id")
+    change_parser.add_argument("--name", required=False, help="Tag name")
+    change_parser.add_argument("--color", required=False, help="Tag color")
+
+    return tag_parser
+
+
 def main(args):
     # Arg parser
     parser = argparse.ArgumentParser(description="Mission CLI")
@@ -227,6 +359,8 @@ def main(args):
     mission_parser = mission_arg_parser(subparser)
 
     folder_arg_parser(subparser)
+
+    tag_parser = tag_arg_parser(subparser)
 
     args = parser.parse_args(args)
 
@@ -256,6 +390,8 @@ def main(args):
                     mission_parser.print_help()
         case "addfolder":
             add_mission_from_folder(args.path, args.location, args.other)
+        case "tag":
+            tag_command(tag_parser, args)
         case _:
             parser.print_help()
 
