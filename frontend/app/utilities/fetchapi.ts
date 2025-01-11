11ace3b614d1d3eb187e8594e2a6ea762e6dd208
Simon Rappenecker <simi2657@gmail.com>
2025-01-11 20:23:08 +0100

    fixed error happend during merge conflict

--- frontend/app/utilities/fetchapi.ts
+++ frontend/app/utilities/fetchapi.ts
@@ -1,4 +1,4 @@
-import { MissionData , Tag, DetailViewData} from "~/data";
+import { MissionData, Tag, DetailViewData } from "~/data";
 import { FETCH_API_BASE_URL } from "~/config";
 
 const headers: {
@@ -74,13 +74,13 @@ export const updateMission = async (
 
 // Function to delete a mission by ID
 export const deleteMission = async (id: number): Promise<void> => {
-    const response = await fetch(`${FETCH_API_BASE_URL}/missions/${id}`, {
-        method: 'DELETE',
-        headers: headers,
-    });
-    if (!response.ok) {
-        throw new Error(`Failed to delete mission with id ${id}`);
-    }
+  const response = await fetch(`${FETCH_API_BASE_URL}/missions/${id}`, {
+    method: "DELETE",
+    headers: headers,
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to delete mission with id ${id}`);
+  }
 };
 
 // Get all tags
@@ -125,17 +125,25 @@ export const addTagToMission = async (
 
 // Remove a tag from a mission
 // Remove a tag from a mission
-export const removeTagFromMission = async (missionId: number, tagName: string): Promise<void> => {
-    const response = await fetch(`${FETCH_API_BASE_URL}/mission-tags/delete/${missionId}/${encodeURIComponent(encodeURIComponent(tagName))}`, {
-        method: 'DELETE',
-        headers: headers,
-    });
-    if (!response.ok) {
-        if (response.status === 404) {
-            throw new Error('Mission, tag, or relationship not found.');
-        }
-        throw new Error('Failed to remove tag from mission');
+export const removeTagFromMission = async (
+  missionId: number,
+  tagName: string
+): Promise<void> => {
+  const response = await fetch(
+    `${FETCH_API_BASE_URL}/mission-tags/delete/${missionId}/${encodeURIComponent(
+      encodeURIComponent(tagName)
+    )}`,
+    {
+      method: "DELETE",
+      headers: headers,
     }
+  );
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Error("Mission, tag, or relationship not found.");
+    }
+    throw new Error("Failed to remove tag from mission");
+  }
 };
 
 // Change the color of a tag
@@ -213,6 +221,29 @@ export const deleteTag = async (tagName: string): Promise<void> => {
 };
 
 // Get all tags for a mission
+export const getTagsByMission = async (missionId: number): Promise<Tag[]> => {
+  const response = await fetch(
+    `${FETCH_API_BASE_URL}/missions/tags/${missionId}`,
+    {
+      method: "GET",
+      headers: headers,
+    }
+  );
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Error("Mission not found");
+    }
+    throw new Error("Failed to fetch tags for mission");
+  }
+  const data = await response.json();
+  return data.map((tag: any) => ({
+    tagId: tag.id,
+    name: tag.name,
+    color: tag.color,
+  }));
+};
+
+// Get all tags for a mission
 export const getMissionsByTag = async (
   tagName: string
 ): Promise<{ id: number; name: string; location: string }[]> => {
@@ -272,97 +303,114 @@ export const attemptLogout = async (
 
 // Helper function to transform durations from seconds to hh:mm:ss
 function transformDurations(durations: string[]): string[] {
-    return durations.map(duration => {
-        const totalSeconds = parseInt(duration, 10);
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
-        
-        return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    })
+  return durations.map((duration) => {
+    const totalSeconds = parseInt(duration, 10);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+      2,
+      "0"
+    )}:${String(seconds).padStart(2, "0")}`;
+  });
 }
 
 // Helper function to transform sizes from bytes to megabytes
 function transformSizes(sizes: string[]): string[] {
-    return sizes.map(size => {
-        const bytes = parseInt(size, 10);
-        const kilobytes = bytes / 1024;
-        const megabytes = kilobytes / 1024;
-        const gigabytes = megabytes / 1024;
+  return sizes.map((size) => {
+    const bytes = parseInt(size, 10);
+    const kilobytes = bytes / 1024;
+    const megabytes = kilobytes / 1024;
+    const gigabytes = megabytes / 1024;
 
-        let value = bytes;
-        let unit = 'B';
+    let value = bytes;
+    let unit = "B";
 
-        if (megabytes < 1) { value = kilobytes; unit = 'KB'; }
-        else if (gigabytes < 1) { value = megabytes; unit = 'MB'; }
-        else { value = gigabytes; unit = 'GB' }
+    if (megabytes < 1) {
+      value = kilobytes;
+      unit = "KB";
+    } else if (gigabytes < 1) {
+      value = megabytes;
+      unit = "MB";
+    } else {
+      value = gigabytes;
+      unit = "GB";
+    }
 
-        return `${value.toFixed(2)} ${unit}`
-    });
+    return `${value.toFixed(2)} ${unit}`;
+  });
 }
 
 // Get details by mission
-export const getDetailsByMission = async (missionId: number): Promise<DetailViewData> => {
-    const response = await fetch(`${FETCH_API_BASE_URL}/missions/${missionId}/files/`,{
-        method: 'GET',
-        headers: headers,
-    });
-    if (!response.ok) {
-        if (response.status === 404) {
-            throw new Error(`Details of mission with ID ${missionId} not found`);
-        }
-        throw new Error(`Failed to fetch details by mission ID ${missionId}`);
+export const getDetailsByMission = async (
+  missionId: number
+): Promise<DetailViewData> => {
+  const response = await fetch(
+    `${FETCH_API_BASE_URL}/missions/${missionId}/files/`,
+    {
+      method: "GET",
+      headers: headers,
     }
-    
-    const data = await response.json();
-
-    const files: string[] = [];
-    const durations: string[] = [];
-    const sizes: string[] = [];
-
-    for (const d in data) {
-        files.push(data[d].file.file_path);
-        durations.push(data[d].file.duration);
-        sizes.push(data[d].file.size);
+  );
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Error(`Details of mission with ID ${missionId} not found`);
     }
+    throw new Error(`Failed to fetch details by mission ID ${missionId}`);
+  }
 
-    return { files, durations, sizes }
+  const data = await response.json();
+
+  const files: string[] = [];
+  const durations: string[] = [];
+  const sizes: string[] = [];
+
+  for (const d in data) {
+    files.push(data[d].file.file_path);
+    durations.push(data[d].file.duration);
+    sizes.push(data[d].file.size);
+  }
+
+  return { files, durations, sizes };
 };
 
 // Get details by mission in correct format
-export const getFormattedDetails = async (missionId: number): Promise<DetailViewData> => {
-    const details = await getDetailsByMission(missionId);
+export const getFormattedDetails = async (
+  missionId: number
+): Promise<DetailViewData> => {
+  const details = await getDetailsByMission(missionId);
 
-    const files = details.files;
-    // transform durations and sizes to correct form
-    const durations = transformDurations(details.durations);
-    const sizes = transformSizes(details.sizes);
+  const files = details.files;
+  // transform durations and sizes to correct form
+  const durations = transformDurations(details.durations);
+  const sizes = transformSizes(details.sizes);
 
-    return { files, durations, sizes }
-}
+  return { files, durations, sizes };
+};
 
 // Get total duration of all files in a mission by ID
 export const getTotalDuration = async (missionId: number): Promise<string> => {
-    const details = await getDetailsByMission(missionId);
+  const details = await getDetailsByMission(missionId);
 
-    let total = 0;
-    for (const duration in details.durations) {
-        total += parseInt(details.durations[duration], 10);
-    }
-    const stringTotal = [`${total}`];
+  let total = 0;
+  for (const duration in details.durations) {
+    total += parseInt(details.durations[duration], 10);
+  }
+  const stringTotal = [`${total}`];
 
-    return transformDurations(stringTotal)[0];
-}
+  return transformDurations(stringTotal)[0];
+};
 
 // Get total duration of all files in a mission by ID
 export const getTotalSize = async (missionId: number): Promise<string> => {
-    const details = await getDetailsByMission(missionId);
-    
-    let total = 0;
-    for (const size in details.sizes) {
-        total += parseInt(details.sizes[size], 10);
-    }
-    const stringTotal = [`${total}`];
+  const details = await getDetailsByMission(missionId);
 
-    return transformSizes(stringTotal)[0];
-}
+  let total = 0;
+  for (const size in details.sizes) {
+    total += parseInt(details.sizes[size], 10);
+  }
+  const stringTotal = [`${total}`];
+
+  return transformSizes(stringTotal)[0];
+};
