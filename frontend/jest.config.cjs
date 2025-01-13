e96842381c2a40cebfe44c86e3796120659bb016
Pascal Kuppler <pkuppler26@gmail.com>
2025-01-13 23:31:29 +0100

    remove unused config and packages

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,34 +1,19 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
-    moduleNameMapper: {
-      '^~/(.*)$': '<rootDir>/app/$1',
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-    },
-    testMatch: [
-      '<rootDir>/app/**/*.test.{ts,tsx}'
-    ],
-    transform: {
-      '^.+\\.(ts|tsx)$': [
-        'ts-jest',
-        {
-          diagnostics: {
-            ignoreCodes: [1343]
-          },
-          astTransformers: {
-            before: [
-              {
-                path: 'node_modules/ts-jest-mock-import-meta',  // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
-                options: { metaObjectReplacement: { env: {} } } // mock import.meta.env
-              }
-            ]
-          }
-        }
-      ]
-    },
-    collectCoverageFrom: [
-      'app/**/*.{ts,tsx}',
-      '!app/**/*.d.ts'
-    ]
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/app/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  testMatch: [
+    '<rootDir>/app/**/*.test.{ts,tsx}'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    '!app/**/*.d.ts'
+  ]
+};
