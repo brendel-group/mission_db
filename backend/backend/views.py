debf8700ac6eaf23e30cb41fcad92754c150446a
Pascal Kuppler <pkuppler26@gmail.com>
2025-01-26 16:14:47 +0100

    add internal documentation

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -12,6 +12,18 @@ import string
 
 
 def authenticate(sessionid: str):
+    """
+    Takes a sessionid as string a confirms the session exists and the user is authenticated
+
+    Args:
+        sessionid (str): sessionid as in the sessionid cookie or session_key in the database
+
+    Raises:
+        PermissionDenied: when session does not exist or user not authenticated
+
+    Returns:
+        User: The user associated with the session
+    """
     try:
         session = Session.objects.get(session_key=sessionid)
     except Session.DoesNotExist:
@@ -28,6 +40,21 @@ def authenticate(sessionid: str):
 
 
 def download(request: HttpRequest, file_path: str):
+    """
+    View that handles downloads. Can handle complete file, single-part and multi-part range requests.
+
+    Args:
+        request (HttpRequest): http request optionally containing a range header and sessionid as url parameter
+        file_path (str): The requested file
+
+    Returns:
+        403 Response: if session not found or user not authenticated
+        404 Response: if file not found
+        200 Response: if whole file requested. Contains the whole file as bytes
+        201 Response: if range requested. A single-part range contains the requested byte range as body.
+            A multi part range has a MimeType multipart body which consists of multiple parts with their own headers.\\
+            The bodies of these parts are the requested byte ranges
+    """
     # try to find user by sessionid
     if not settings.DEBUG:
         _ = authenticate(request.GET["sessionid"])
@@ -50,6 +77,16 @@ def download(request: HttpRequest, file_path: str):
 
 
 def _extract_ranges(range_header: str, file_size: int):
+    """Parses the range header and converts the ranges to python ranges
+    Note: ranges in header are inclusive while python ranges are not inclusive on the upper bound
+
+    Args:
+        range_header (str): content of range header as string
+        file_size (int): the maximum file size
+
+    Returns:
+        list[range]: List of the extracted ranges
+    """
     ranges = range_header.split(",")
     new_ranges = []
     for r in ranges:
@@ -65,6 +102,15 @@ def _extract_ranges(range_header: str, file_size: int):
 
 
 def _range_download(request: HttpRequest, file: File):
+    """Handles downloads if a range is requested
+
+    Args:
+        request (HttpRequest): The original request with the range header
+        file (File): The requested file
+
+    Returns:
+        201 Response: The requested byte ranges. Either as single range or multipart response with multiple ranges.
+    """
     if "bytes" in request.headers["range"]:
         range_header: str = (
             request.headers["range"].replace("bytes=", "").replace(" ", "")
@@ -105,6 +151,18 @@ def _range_download(request: HttpRequest, file: File):
 
 
 def _multipart_range_download(ranges: list[range], file: File):
+    """Handles multipart range requests\\
+    The response consists of multiple parts which each having their own headers and bodies.\\
+    The parts are separated by a boundary that is declared in the main headers.\\
+    The boundary is a random 13 digit string
+
+    Args:
+        ranges (list[range]): requested ranges
+        file (File): requested file
+
+    Returns:
+        201 Response: The mutlipart response with the different byte ranges.
+    """
     range_sizes: list[int] = [len(r) for r in ranges]
 
     content_length: int = sum(range_sizes)
@@ -152,6 +210,19 @@ def _multipart_range_download(ranges: list[range], file: File):
 def _chunk_generator(
     file: File, r: range, chunk_size: int = File.DEFAULT_CHUNK_SIZE, close: bool = False
 ):
+    """Takes a file and range and acts as an iterator that reads the file in chunks until the end of the range is reached.\\
+    Starts reading tha file at the start of the range and not the start of the file.\\
+    Optionally can close the file after reading the last chunk.
+
+    Args:
+        file (File): The file from which to read
+        r (range): The range in bytes to read.
+        chunk_size (int, optional): The size of chunks in which the file will be read. Defaults to File.DEFAULT_CHUNK_SIZE.
+        close (bool, optional): Close the file after reading last chunk. Defaults to False.
+
+    Yields:
+        chunk_size large byte blocks read from the file
+    """
     try:
         file.seek(r.start)
         for i in range(int(len(r) / chunk_size)):
@@ -167,6 +238,18 @@ def _chunk_generator(
 
 
 def _body_generator(file: File, body: list[Iterable[bytes]], boundary: str):
+    """Takes the body of the multipart range response as a list of iterators and acts like a single iterator,\\
+    so that this can be passed to the StreamingHttpResponse.\\
+    Closes the file when the last boundary is reached.    
+
+    Args:
+        file (File): The opened file to close it at the end.
+        body (list[Iterable[bytes]]): The body as list of iterators.
+        boundary (str): The boundary separating the multipart bodies. To check when the end is reached.
+
+    Yields:
+        The body parts either line by line (header) or in byte blocks.
+    """
     for chunks in body:
         for chunk in chunks:
             if chunk == f"\r\n--{boundary}--\r\n":
