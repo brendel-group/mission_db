f54327875a1b969767463b3b1ba59a81dba2db73
KageKrito <148358692+KageKrito@users.noreply.github.com>
2025-02-19 10:47:24 +0100

    Login feedback bug (#255)
    This PR resolves #254 
    #
    ### Changes
    I fixed the bug mentioned in the linked issue by disabling the button
    while logging in.
    I also fixed the gitter happening during the animation by seperating the
    static part of the text from the dynamic/animated part.
    
    ### Testing
    Can be tested by quickly pressing the button repeatedly during the login
    process
    - Currently in main, this results in the dots speeding up
    - Now the button cant be pressed while already logging in, therefor the
    dots won't speed up anymore

--- frontend/app/pages/login/LoginPage.tsx
+++ frontend/app/pages/login/LoginPage.tsx
@@ -9,37 +9,38 @@ import {
 import classes from "./LoginPage.module.css";
 import { Form } from "@remix-run/react";
 import { IconInfoCircle } from "@tabler/icons-react";
+import { useState } from "react";
 
 export function LoginView({ error }: { error?: string }) {
 
+  const [buttonClickable, setButtonClickable] = useState(true); // State to control button clickability
+  const [buttonText, setButtonText] = useState("Log in"); // State to control button text
+  const [dots, setDots] = useState(0); // State to control the number of dots
+  const numberOfDots = 3; // Number of dots to be shown
+
   const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault(); // Prevent the form from submitting immediately
+    setButtonClickable(false); // Disable the button to prevent multiple submissions
+    document.querySelector('.mantine-Alert-root')?.remove(); // Remove any existing alerts
+    setButtonText("Logging in"); // Change the button text to "Logging in"
+
     const button = e.currentTarget; // Get the button that was clicked
     const form = button.closest('form');
-    
-    e.preventDefault(); // Prevent the form from submitting immediately
-    document.querySelector('.mantine-Alert-root')?.remove(); // Remove any existing alerts
     form?.submit(); // Submit the form
 
-    button.innerHTML = 'Logging in&nbsp;.&nbsp;.&nbsp;.'; // Set the button text to "Logging in . . ." to indicate loading
-
     const delay = 300; // Change the dots every 300ms
-    const numberOfDots = 3; // Number of dots to be shown
-    let dots = 0;
-    let spaces = numberOfDots;
 
     const interval = setInterval(() => {
-      dots = (dots + 1) % (numberOfDots + 1);
-      spaces = numberOfDots - dots;
-      button.innerHTML = `Logging in${'&nbsp;.'.repeat(dots)}${'&nbsp;&nbsp;'.repeat(spaces)}`; // Animate the dots
+      setDots((dot) => (dot + 1) % (numberOfDots + 1)); // Animate the dots
 
       const isValid = form?.checkValidity();
       const alert = document.querySelector('.mantine-Alert-root');
       
       if (!isValid || (alert !== null)) { // End if the form is invalid or an alert is shown
         clearInterval(interval); // Stop the animation
-        button.innerText = 'Log in'; // Reset the button text
+        setButtonText("Log in"); // Reset the button text
+        setButtonClickable(true); // Enable the button
       }
-
     }, delay);
   };
 
@@ -104,8 +105,14 @@ export function LoginView({ error }: { error?: string }) {
               gradient={{ from: "yellow", to: "orange", deg: 269 }}
               className={classes.login_button}
               onClick={handleSubmit}
+              disabled={!buttonClickable}
             >
-              Log in
+              <span style={{ marginLeft: "1.3em" }}>{buttonText} </span>
+              {/* Add space to center the text */}
+              <span style={{ display: "inline-block", textAlign: "left", marginLeft: "0.3em", width: "1.3em" }}>
+                {". ".repeat(dots)}
+              </span>
+              {/* Add space to separate the text from the dots */}
             </Button>
           </Paper>
         </div>
