ef523b078e089d011fe67951607775710833fc66
Felix Mycka <felix04m@gmail.com>
2025-02-21 17:13:10 +0100

    fixed bugs with mono16 encoding

--- backend/rosdecoder.py
+++ backend/rosdecoder.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import cv2
 from rosbags.highlevel import AnyReader
 from rosbags.typesys import Stores, get_typestore
+import numpy as np
 
 bagpath = Path(r"")
 
@@ -10,7 +11,9 @@ typestore = get_typestore(Stores.ROS2_FOXY)
 
 
 def get_video_topics(path):
+    # Open the bag file using AnyReader
     with AnyReader([path], default_typestore=typestore) as reader:
+        # Extract topics that have message type "sensor_msgs/msg/Image"
         connections = [
             x.topic for x in reader.connections if x.msgtype == "sensor_msgs/msg/Image"
         ]
@@ -23,23 +26,38 @@ def get_video_data(path, topic):
     width = 0
     height = 0
     step = 0
+    # Open the bag file using AnyReader
     with AnyReader([bagpath], default_typestore=typestore) as reader:
+        # Filter connections for the specified topic
         connections = [x for x in reader.connections if x.topic == topic]
+        # Iterate through messages in the filtered connections
         for connection, timestamp, rawdata in reader.messages(connections=connections):
+            # Deserialize the raw data to get the message
             msg = reader.deserialize(rawdata, connection.msgtype)
             width = msg.width
             height = msg.height
             step = msg.step
-            data.append(msg.data.reshape(height, width, int(step / width)))
+            if msg.encoding == "mono16":
+                # Convert raw bytes to numpy 16-bit grayscale image
+                tmp = np.frombuffer(msg.data, dtype=np.uint16).reshape((height, width, 1))
+
+                # Normalize to 8-bit range (0-255) for visualization
+                normalized = (tmp / 256).astype(np.uint8)
+
+                data.append(normalized)
+            else:
+                # Normal RGB or Mono8 case
+                data.append(np.frombuffer(msg.data, dtype=np.uint8).reshape(height, width, int(step / width)))
+
     return data, topic
 
 
 def create_video(data, topic):
-    print(data)
-    print(data[0].shape)
+    # Print data and shape of the first frame for debugging
     height, width, channels = data[0].shape
+    # Create a filename based on the topic name
     filename = str(topic).replace("/", "-") + ".mp4"
-    print(filename)
+    # Initialize the video writer
     video = cv2.VideoWriter(
         filename,
         cv2.VideoWriter_fourcc(*"mp4v"),
@@ -48,9 +66,11 @@ def create_video(data, topic):
         isColor=channels == 3,
     )
 
+    # Write each frame to the video file
     for frame in data:
         video.write(frame)
 
+    # Release the video writer
     video.release()
 
 
